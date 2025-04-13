ea0905c937e20a1ce477618580f6cd971396844a
arkajyotibose003 <arkajyotibose003@gmail.com>
2025-04-13 10:05:38 +0530

    AIftecher

--- AIfetcher/app.py
+++ AIfetcher/app.py
@@ -2,11 +2,25 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import google.generativeai as genai
 
+
+import os
+
+# Get path to the parent directory (AI_Chatbot/)
+base_dir = os.path.dirname(os.path.dirname(__file__))
+key_path = os.path.join(base_dir, "apikeys.txt")
+
+with open(key_path) as f:
+    lines = f.readlines()
+    api_key = lines[5].strip()  # line 6 (index 5)
+
+
+
 app = Flask(__name__)
 CORS(app)  # Allow cross-origin requests
 
 # Set your Gemini API Key
-genai.configure(api_key="apikey")
+
+genai.configure(api_key="AIzaSyCShjJfSgtqGMMnqC73g094mLScv1L4bHA")
 
 @app.route('/chat', methods=['POST'])
 def chat():
