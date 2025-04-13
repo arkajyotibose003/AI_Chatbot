15416e7d3538a7122100681d51eac3cbaf3cba7a
Arkajyoti Bose <arkajyotibose003@gmail.com>
2025-04-13 10:32:59 +0530

    Update app.py

--- AIfetcher/app.py
+++ AIfetcher/app.py
@@ -3,18 +3,6 @@ from flask_cors import CORS
 import google.generativeai as genai
 
 
-import os
-
-# Get path to the parent directory (AI_Chatbot/)
-base_dir = os.path.dirname(os.path.dirname(__file__))
-key_path = os.path.join(base_dir, "apikeys.txt")
-
-with open(key_path) as f:
-    lines = f.readlines()
-    api_key = lines[5].strip()  # line 6 (index 5)
-
-
-
 app = Flask(__name__)
 CORS(app)  # Allow cross-origin requests
 
